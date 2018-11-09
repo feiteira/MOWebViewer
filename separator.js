4ba1a4e36c57a44fa2023c5aab3f93f2ec1a2974
Jose Luis Feiteirinha <feiteira@gmail.com>
2018-11-09 21:29:43 +0100

    fixup! #10 Enables moving of vertical separator / resizeing of Tree width.

--- separator.js
+++ separator.js
@@ -1,32 +1,20 @@
 window.separator_mode = "PAUSED";
 
-console.log("loadin");
-
-/**
-
-$(document).ready(function () {
-  console.log($("#v_separator"));
-  $("#v_separator").click(function handle_separator(){
-    console.log(window.separator_mode);
+$(document).mousedown(function (event){
+  if ($('#v_separator:hover').length != 0) {
     window.separator_mode = "RUNNING"
-    console.log(window.separator_mode);
-
-  });**/
+  }
+});
 
 $(document).mousemove(function (event){
-  console.log("a "+ window.separator_mode)
   if(window.separator_mode == "RUNNING"){
     console.log(event.pageX*100.0/$(window).width());
     $("#div_left_panel").width(event.pageX*100.0/$(window).width()+ "%");
   }
 });
 
-$(document).click(function (event){
+$(document).mouseup(function (event){
   if(window.separator_mode == "RUNNING"){
     window.separator_mode = "PAUSED";
-  }else{
-    if ($('#v_separator:hover').length != 0) {
-      window.separator_mode = "RUNNING"
-    }
   }
 });
